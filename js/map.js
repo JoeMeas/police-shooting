44492091ca8cbc4bcc6eade83256336272f4ed4f
JoeMeas <perrylinmeas@gmail.com>
2015-07-05 15:01:31 -0700

    Early test build

--- js/map.js
+++ js/map.js
@@ -1,26 +1,39 @@
-// Function to draw your map
-var drawMap() = function() {
 
+// Function to draw your map
+var drawMap = function() {
   // Create map and set viewd
- 
+  var map = L.map('container');
+  map.setView([47.6, -122.3], 12);
+
 
   // Create an tile layer variable using the appropriate url
-
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
   // Add the layer to your map
- 
+  layer.addTo(map);
 
   // Execute your function to get data
- 
+  getData(map);
 }
 
 // Function for getting data
-var getData = function() {
+var getData = function(map) {
 
   // Execute an AJAX request to get the data in data/response.js
-
+  var data;
+  $.ajax({
+    url:'./data/response.json',
+    type: "get",
+    success:function(dat) {
+       data = dat;
+       data.map(function(d){
+         var circle = new L.circle([d.latitude, d.longitude], 200, {color:'red', opacity:.5}).addTo(map)
+       })
+       customBuild();
+    }, 
+    dataType:"json"
+  }) 
 
   // When your request is successful, call your customBuild function
-
 }
 
 // Do something creative with the data here!  
